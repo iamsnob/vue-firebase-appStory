655cdf5d8f6af0ca6ffc32e683ce02a56c48f35e
iamsnob <mrsnobdecoder@gmail.com>
2020-05-24 20:43:45 +0800

    initial

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,5 @@ module.exports = {
   "transpileDependencies": [
     "vuetify"
   ],
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/iamsnob.github.io/'
-    : '/'
+ // publicPath: process.env.NODE_ENV === 'production'? '/iamsnob.github.io/' : '/'
 }
